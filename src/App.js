6563ec3b59f0d8814349dc34d7da63c7defb3820
AMELIE GUILLOUX <amelie.guilloux@hotmail.fr>
2022-11-18 16:16:23 +0100

    card created

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+        <h2>Title</h2>
+        <div className="testImg" ></div>
+        <p className="description">
+          Lorem ipsum dolor sit amet. Et nihil aspernatur ut libero veniam quo consequatur aliquid in molestias deserunt ut voluptatem molestiae et praesentium eius.
+        </p>
+        <hr />
+        <h3>Subtitle</h3>
+        <p>Lorem ipsum dolor sit amet. Et nihil aspernatur ut libero veniam quo consequatur aliquid in molestias deserunt ut voluptatem molestiae et praesentium eius. </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
